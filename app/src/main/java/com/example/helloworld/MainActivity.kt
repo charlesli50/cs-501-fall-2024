efc8393ad284205a9869c50bb468bb7af7629ca0
charlesli50 <charles.zhiyu.li@gmail.com>
2024-09-05 22:51:11 -0400

    added click button functionality

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -8,7 +8,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.helloworld.ui.theme.HelloworldTheme
@@ -32,10 +35,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello World!",
-        modifier = modifier
-    )
+
+    var clicked by remember { mutableStateOf(false) }
+
+    Button(onClick = { clicked = true }) {
+        Text("Click Me")
+    }
+
+    if (clicked) {
+        Text(
+            text = "                          Hello World!",
+            modifier = modifier
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
